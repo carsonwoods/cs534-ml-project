18d39e4eab29da71157557dbd09c9d62d410bc4f
Carson Woods <cwoods289@gmail.com>
2023-11-25 11:28:08 -0500

    Update docstring

--- read_data.py
+++ read_data.py
@@ -18,7 +18,7 @@ def get_data_df():
 
 def get_data_numpy():
     """
-    Reads CSV into pandas dataframe from data directory
+    Reads CSV into numpy array from data directory
     """
     data_path = Path("./data/diabetic_data.csv")
     return pd.read_csv(data_path).to_numpy()
