c754533c7aaee98b1b01dcd5e24e47cd4c8bd550
Carson Woods <cwoods289@gmail.com>
2023-11-25 11:27:50 -0500

    Add new function to read data as numpy array

--- read_data.py
+++ read_data.py
@@ -5,6 +5,7 @@ Handles reading the data using a consistent interface.
 from pathlib import Path
 
 import pandas as pd
+import numpy as np
 
 
 def get_data_df():
@@ -15,5 +16,13 @@ def get_data_df():
     return pd.read_csv(data_path)
 
 
+def get_data_numpy():
+    """
+    Reads CSV into pandas dataframe from data directory
+    """
+    data_path = Path("./data/diabetic_data.csv")
+    return pd.read_csv(data_path).to_numpy()
+
+
 if __name__ == "__main__":
-    print(get_data_df())
+    print(get_data_numpy())
