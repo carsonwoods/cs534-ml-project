d88261eaa5e13dc89eb544511fe1f05156662c36
Carson Woods <cwoods289@gmail.com>
2023-11-25 10:53:33 -0500

    Add docstring

--- read_data.py
+++ read_data.py
@@ -1,10 +1,19 @@
+"""
+Handles reading the data using a consistent interface. 
+"""
+
 from pathlib import Path
 
 import pandas as pd
 
+
 def get_data_df():
+    """
+    Reads CSV into pandas dataframe from data directory
+    """
     data_path = Path("./data/diabetic_data.csv")
     return pd.read_csv(data_path)
+
 
 if __name__ == "__main__":
     print(get_data_df())
