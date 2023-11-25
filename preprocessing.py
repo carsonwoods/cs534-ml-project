d695a3c0b89aec335cf531cd52a01f24006f43e8
Carson Woods <cwoods289@gmail.com>
2023-11-25 12:19:19 -0500

    Adds lots of new preprocessing options

--- preprocessing.py
+++ preprocessing.py
@@ -1,10 +1,13 @@
 """
 Various preprocessing functions
 """
+# 1st party imports
+import sys
 
 # 3rd party imports
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import StandardScaler
 
 
 def get_feature_labels(data):
@@ -14,37 +17,78 @@ def get_feature_labels(data):
     x -> features
     y -> labels
     """
-    if type(data) == np.ndarray:
+    if isinstance(data, np.ndarray):
         return data[:, :-1], data[:, -1]
-    elif type(data) == pd.DataFrame:
+    if isinstance(data, pd.DataFrame):
         data_arr = data.to_numpy()
         return data_arr[:, :-1], data_arr[:, -1]
+
+    print(
+        f"Error: data parameter is of unsupported datatype: {type(data)}"
+    )
+    sys.exit(1)
+
+
+def factorize(x):
+    """
+    Handles preprocessing of non-numeric datatypes
+    this will not impact numeric data
+    Returns:
+        x_factorized -> ndarray in the original shape of x with
+                        non-numeric features encoded via factorization
+    """
+    if isinstance(x, np.ndarray):
+        df = pd.DataFrame(x)
+    elif isinstance(x, pd.DataFrame):
+        df = x
     else:
         print(
-            "Error: data parameter is of unknown datatype: {0}".format(
-                type(data)
-            )
+            f"Error: x parameter is of unsupported datatype: {type(x)}"
         )
-        exit()
+        sys.exit(1)
 
+    df = df.convert_dtypes()
+    for col_idx in df:
+        if (df[col_idx].dtype) == "string":
+            df[col_idx] = pd.factorize(df[col_idx])[0]
+
+    return df.to_numpy()
+
+
+def standardize_features(x, return_scaler=True):
+    """
+    Uses sklearn to perform standard scaling on features.
+    With scaler being returned as well to apply transformation to
+    a different dataset (fit model on train, apply to test, etc.)
+    Params:
+        x             -> the features of the data to be scaled
+        return_scaler -> determines if the scaler object should be returned
+    Returns:
+        x_transformed -> the standardizes input data
+        scaler        -> the scaling model
+    """
+    scaler = StandardScaler()
+    scaler.fit(x)
+    if return_scaler:
+        return scaler.transform(x), scaler
+    else:
+        return scaler.transform(x)
 
 if __name__ == "__main__":
-    """
-    This code is just some testing to ensure that the functions work correctly.
-    This won't run unless running preprocessing.py directly
-    """
+    # This code is just some testing to ensure that the functions work correctly.
+    # This won't run unless running preprocessing.py directly
 
     # imports from other parts of the project
     import read_data
 
-    data_np = read_data.get_data_numpy()
-    x, y = get_feature_labels(data_np)
-    print(data_np)
-    print(x)
-    print(y)
-
     data_df = read_data.get_data_df()
-    x, y = get_feature_labels(data_df)
+    data_x, data_y = get_feature_labels(data_df)
     print(data_df)
-    print(x)
-    print(y)
+    print(data_x)
+    print(data_y)
+
+    # try factorizing
+    data_x = factorize(data_x)
+
+    x_transform = standardize_features(data_x, return_scaler=False)
+    print(x_transform)
