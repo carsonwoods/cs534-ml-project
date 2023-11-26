786a748f672bf87041de5c9ce23cddd4bd9111cb
Carson Woods <cwoods289@gmail.com>
2023-11-26 15:35:00 -0500

    Add some feature selection functions

--- diabetes_project/preprocessing.py
+++ diabetes_project/preprocessing.py
@@ -8,6 +8,8 @@ import sys
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+from scipy.stats import spearmanr
+from sklearn.metrics import mutual_info_score
 
 
 def get_feature_labels(data):
@@ -30,7 +32,9 @@ def get_feature_labels(data):
 def factorize(x):
     """
     Handles preprocessing of non-numeric datatypes
-    this will not impact numeric data
+    this will not impact numeric data.
+    If a 1-D array (such as labels) is passed in, the output
+    will be flattened again to a single dimension.
     Returns:
         x_factorized -> ndarray in the original shape of x with
                         non-numeric features encoded via factorization
@@ -48,7 +52,11 @@ def factorize(x):
         if (df[col_idx].dtype) == "string":
             df[col_idx] = pd.factorize(df[col_idx])[0]
 
-    return df.to_numpy()
+    array = df.to_numpy()
+    if array.shape[1] == 1:
+        return array.flatten()
+    else:
+        return array
 
 
 def filter_pairwise_corr_vars(x, corrtype, threshold=0.60):
@@ -61,9 +69,7 @@ def filter_pairwise_corr_vars(x, corrtype, threshold=0.60):
         x_fs -> numpy array containing only the features
                 which survived the selection
     """
-    if (
-        corrtype not in ('pearson', 'kendall', 'spearman')
-    ):
+    if corrtype not in ("pearson", "kendall", "spearman"):
         print("Error: must choose from pearson, kendall, or spearman corrtype")
         sys.exit(1)
 
@@ -83,6 +89,71 @@ def filter_pairwise_corr_vars(x, corrtype, threshold=0.60):
                 remove_list.add(index[1])
 
     return np.delete(x, list(remove_list), 1)
+
+
+def filter_most_corr(x, y, rank_type="correlation", threshold=0.80):
+    """
+    Removes features with a correlation or mutual information
+    score of >= threshold from the list of features in x
+    Params:
+        x -> original features
+        y -> original labels
+        rank_type -> "correlation" or "mutual"
+        threshold -> "cutoff for feature selection"
+    Returns:
+        x_fs -> ndarray of x with filtered features removed
+    """
+    rankings = []
+    if rank_type == "correlation":
+        corr_results = dict()
+        # ensures that the type is an integer
+        # so a classifier can be applied
+        # y = y.astype(int)
+
+        # iterate across all features (columns) in x
+        for col_idx in range(0, len(x[0])):
+            corr = abs(spearmanr(x[:, col_idx], y).statistic)
+            if corr not in corr_results:
+                corr_results[corr] = [col_idx]
+            else:
+                corr_results[corr].extend([col_idx])
+
+        for key in sorted(corr_results, reverse=True):
+            rankings.extend(corr_results[key])
+    elif rank_type == "mutual":
+        corr_results = dict()
+
+        # ensures that the type is an integer
+        # so a classifier can be applied
+        y = y.astype(int)
+
+        # iterate across all features (columns) in x
+        for col_idx in range(0, len(x[0])):
+            corr = abs(mutual_info_score(x[:, col_idx], y))
+            if corr not in corr_results:
+                corr_results[corr] = [col_idx]
+            else:
+                corr_results[corr].extend([col_idx])
+
+        for key in sorted(corr_results, reverse=True):
+            rankings.extend(corr_results[key])
+    else:
+        print("Error: must choose correlation or mutual for rank_type")
+        sys.exit(1)
+
+    if threshold > 1 or threshold < 0:
+        print("Error: threshold must be between 0 and 1")
+
+    new_rankings = []
+    for idx in range(0, int(len(rankings) * threshold) - 1):
+        new_rankings.append(rankings[idx])
+
+    delete_list = set()
+    for feature_idx in range(0, x.shape[1]):
+        if feature_idx not in new_rankings:
+            delete_list.add(feature_idx)
+
+    return np.delete(x, list(delete_list), 1)
 
 
 def standardize_features(x, return_scaler=True):
@@ -120,8 +191,9 @@ if __name__ == "__main__":
 
     # try factorizing
     data_x = factorize(data_x)
+    data_y = factorize(data_y)
 
     x_transform = standardize_features(data_x, return_scaler=False)
     print(x_transform.shape)
-    x_transform = filter_pairwise_corr_vars(x_transform, "spearman", .3)
+    x_transform = filter_most_corr(x_transform, data_y, "mutual", 0.8)
     print(x_transform.shape)
