25d8c1a7949eab17b69093f6743d8f67a4feeb39
Carson Woods <cwoods289@gmail.com>
2023-12-08 14:06:48 -0500

    Reduce kNN search space for computing practicality

--- build_knn.py
+++ build_knn.py
@@ -22,9 +22,7 @@ train_x, test_x, train_y, test_y = default_preprocessing(data_df)
 # trains K-NN classifier
 results = {}
 results["params"] = {}
-results["params"]["n_neighbors"] = [1, 2, 3, 4, 5, 10, 15, 20, 25, 50, 100]
-results["params"]["leaf_size"] = [1, 2, 3, 4, 5, 10, 15, 20, 25, 30, 50, 100]
-results["params"]["weights"] = ["uniform", "distance"]
+results["params"]["n_neighbors"] = [5, 10, 15, 20, 25, 50, 100]
 results["params"]["metric"] = [
     "manhattan",
     "euclidean",
