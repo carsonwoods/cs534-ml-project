b2a2b981ae3bcfa137195f64796c87e992467186
Kiersten Campbell <kierstencampbell21@gmail.com>
2023-12-09 14:12:40 -0500

    option to run with z-scored data

--- build_knn.py
+++ build_knn.py
@@ -8,7 +8,7 @@ from sklearn.neighbors import KNeighborsClassifier
 # imports from other parts of the project
 from diabetes_project.read_data import get_data_df
 from diabetes_project.preprocessing import (
-    default_preprocessing, default_preprocessing_pca
+    default_preprocessing, default_preprocessing_pca, default_preprocessing_zscore
 )
 from diabetes_project.model import build_generic_model
 
@@ -45,6 +45,37 @@ results = build_generic_model(
 print(results)
 
 '''
+# Run again with z-scoring included - load df from scratch to avoid any accidental inplace modifications from calls above
+data_df = get_data_df()
+
+# Run full set of preprocessing steps + zscoring
+train_x, test_x, train_y, test_y = default_preprocessing_zscore(data_df)
+
+# trains K-NN classifier
+results = {}
+results["params"] = {}
+results["params"]["n_neighbors"] = [5, 10, 15, 20, 25, 50, 100]
+results["params"]["metric"] = [
+    "manhattan",
+    "euclidean",
+    "cosine",
+    "haversine",
+    "minkowski",
+]
+
+# instantiates model to tune
+knn_model = KNeighborsClassifier()
+results = build_generic_model(
+    knn_model,
+    results["params"],
+    train_x,
+    train_y,
+    test_x,
+    test_y,
+)
+
+print(results)
+
 # Run again with PCA transformed data - load df from scratch to avoid any accidental inplace modifications from calls above
 data_df = get_data_df()
 
